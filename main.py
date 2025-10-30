e562f94e225a4db1bf1c3df2ecb040533ca6042b
Aleksandr Shenshin <aashenshin@yandex.ru>
2025-10-30 12:48:45 +0300

    Refactoring router

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import asyncio
 import logging
 
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher, types, Router
 from aiogram.filters.command import Command
 
 # ВАЖНО! Вставьте сюда ваш токен, полученный от @BotFather
@@ -15,22 +15,28 @@ bot = Bot(token=BOT_TOKEN)
 # Диспетчер
 dp = Dispatcher()
 
-@dp.message(Command("help"))
+# Создаем экземпляр роутера
+router = Router()
+
+@router.message(Command("help"))
 async def cmd_help(message: types.Message):
     await message.answer("Я умею делать следующее...")
 
 # Хэндлер на команду /start
-@dp.message(Command("start"))
+@router.message(Command("start"))
 async def cmd_start(message: types.Message):
     await message.answer("Привет! Я эхо-бот. Отправь мне любое сообщение, и я его повторю.")
 
 # Хэндлер на остальные текстовые сообщения
-@dp.message()
-async def echo_handler(message: types.Message):
+@router.message()
+async def echo_message(message: types.Message):
     await message.answer(f"Я получил твое сообщение: {message.text}")
 
 # Запуск процесса поллинга новых апдейтов
 async def main():
+    # Регистрируем роутер в диспетчере
+    dp.include_router(router)
+    
     # Удаляем вебхук и пропускаем накопившиеся входящие сообщения
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
