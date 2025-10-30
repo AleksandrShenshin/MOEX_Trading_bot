bd70819b5a996cb8ef222b7223a2be86d9c7f8ec
Aleksandr Shenshin <aashenshin@yandex.ru>
2025-10-30 15:02:16 +0300

    Add Inline button and callback

--- handlers/basic_handlers.py
+++ handlers/basic_handlers.py
@@ -1,6 +1,7 @@
-from aiogram import Router, types
+from aiogram import Router, types, F
 from aiogram.filters.command import Command
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
+from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 # Все роутеры нужно именовать так, чтобы не было конфликтов
 router = Router()
@@ -22,7 +23,7 @@ async def cmd_start(message: types.Message):
     builder = ReplyKeyboardBuilder()
     
     # Добавляем кнопки
-    builder.button(text="Start")
+    builder.button(text="/actions")
     builder.button(text="/help")
     
     # Указываем, сколько кнопок будет в одном ряду (в данном случае 2)
@@ -33,7 +34,35 @@ async def cmd_start(message: types.Message):
         reply_markup=builder.as_markup(resize_keyboard=True)
     )
 
+@router.message(Command("actions"))
+async def cmd_actions(message: types.Message):
+    builder = InlineKeyboardBuilder()
+
+    builder.button(text="Показать уведомление", callback_data="show_alert")
+    builder.button(text="Сменить это сообщение", callback_data="edit_message")
+
+    await message.answer(
+        "Нажми на кнопку, чтобы выполнить действие:",
+        reply_markup=builder.as_markup()
+    )
+
 # Хэндлер на остальные текстовые сообщения
 @router.message()
 async def echo_message(message: types.Message):
     await message.answer(f"Я получил твое сообщение: {message.text}")
+
+# Хэндлер для обработки нажатия на кнопку "Показать уведомление"
+@router.callback_query(F.data == "show_alert")
+async def handle_show_alert(callback: types.CallbackQuery):
+    await callback.answer(
+        "Это всплывающее уведомление!",
+        show_alert=True # Делает уведомление модальным окном
+    )
+
+# Хэндлер для обработки нажатия на кнопку "Сменить это сообщение"
+@router.callback_query(F.data == "edit_message")
+async def handle_edit_message(callback: types.CallbackQuery):
+    # Редактируем текст исходного сообщения
+    await callback.message.edit_text("Сообщение было изменено!")
+    # Отвечаем на callback, чтобы убрать "часики" на кнопке
+    await callback.answer()
