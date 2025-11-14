ef16f240533d89995fb60ec27e0735ca940d6b35
Aleksandr Shenshin <aashenshin@yandex.ru>
2025-11-14 23:33:05 +0300

    Change format list tickers to dict

--- general.py
+++ general.py
@@ -1,6 +1,10 @@
+import ast
 import asyncio
 from decouple import config
 
 
 async def get_list_ticker():
-    return config('FUTURES_LIST', cast=lambda v: [s.strip() for s in v.split(',')])
+    try:
+        return ast.literal_eval(config('FUTURES_LIST'))
+    except (SyntaxError, ValueError) as e:
+        return None
